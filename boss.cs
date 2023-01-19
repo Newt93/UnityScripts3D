221056451b98992912e4e76b8c4c77093c246a19
Leah <105241362+Newt93@users.noreply.github.com>
2023-01-18 21:51:50 -0600

    Create boss.cs

---
+++ boss.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+
+public class BossBattle : MonoBehaviour
+{
+    public GameObject boss;
+    public GameObject player;
+
+    private float bossHealth;
+    private float playerDamage;
+    private float bossDamage;
+    private float attackInterval;
+
+    void Start()
+    {
+        bossHealth = 100f;
+        playerDamage = 10f;
+        bossDamage = 20f;
+        attackInterval = 2f; //interval between each attack
+    }
+
+    void Update()
+    {
+        if (boss != null)
+        {
+            if (Input.GetKeyDown(KeyCode.a))
+            {
+                bossHealth -= playerDamage;
+                Debug.Log("Player dealt " + playerDamage + " damage to the boss. Boss health: " + bossHealth);
+            }
+
+            if (bossHealth > 0)
+            {
+                attackInterval -= Time.deltaTime;
+                if (attackInterval <= 0)
+                {
+                    Attack();
+                    attackInterval = 2f;
+                }
+            }
+
+            if (bossHealth <= 0)
+            {
+                Debug.Log("Boss defeated!");
+                Destroy(boss);
+            }
+        }
+    }
+
+    void Attack()
+    {
+        player.GetComponent<PlayerHealth>().TakeDamage(bossDamage);
+        Debug.Log("Boss dealt " + bossDamage + " damage to the player.");
+    }
+}
